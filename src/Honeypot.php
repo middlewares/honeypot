b8ab328b8afee108a03318bfb9c90d2e3d1225c7
oscarotero <oom@oscarotero.com>
2018-10-12 13:40:54 +0200

    Allow to definte the factories in the constructor

--- src/Honeypot.php
+++ src/Honeypot.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 namespace Middlewares;
 
 use Middlewares\Utils\Traits\HasResponseFactory;
+use Middlewares\Utils\Factory;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -46,9 +48,10 @@ class Honeypot implements MiddlewareInterface
     /**
      * Set the field name.
      */
-    public function __construct(string $name = 'hpt_name')
+    public function __construct(string $name = 'hpt_name', ResponseFactoryInterface $responseFactory = null)
     {
         $this->name = self::$currentName = $name;
+        $this->responseFactory = $responseFactory ?: Factory::getResponseFactory();
     }
 
     /**
